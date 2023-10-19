59ef0ba1abd2d718283d623eea457e848c8218df
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-10-19 10:52:05 -0300

    first commit

---
+++ src/main/java/com/anhembi/ironcar/IroncarApplication.java
@@ -0,0 +1,13 @@
+package com.anhembi.ironcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IroncarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IroncarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anhembi/ironcar/IroncarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhembi.ironcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IroncarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
