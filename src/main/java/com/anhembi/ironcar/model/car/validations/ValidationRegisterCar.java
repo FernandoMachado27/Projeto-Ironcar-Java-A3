20a369d06206123507346a745bcffc5c15560230
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-11-22 01:31:45 -0300

    Adicionando validador de ano

---
+++ src/main/java/com/anhembi/ironcar/model/car/validations/ValidationRegisterCar.java
@@ -0,0 +1,9 @@
+package com.anhembi.ironcar.model.car.validations;
+
+import com.anhembi.ironcar.model.car.DataRegistrationCar;
+
+public interface ValidationRegisterCar {
+	
+	void validate(DataRegistrationCar carData);
+
+}
---
+++ src/main/java/com/anhembi/ironcar/model/car/validations/ValidationYear.java
@@ -0,0 +1,22 @@
+package com.anhembi.ironcar.model.car.validations;
+
+import org.springframework.stereotype.Component;
+
+import com.anhembi.ironcar.model.ValidacaoException;
+import com.anhembi.ironcar.model.car.DataRegistrationCar;
+
+@Component
+public class ValidationYear implements ValidationRegisterCar{
+	
+	public void validate(DataRegistrationCar carData) {
+		var yearString = carData.year();
+		int year = Integer.parseInt(yearString);
+		
+		int yearLimit = 2000;
+		
+		if(year < yearLimit) {
+			throw new ValidacaoException("Permitido cadastrar apenas carros fabricados a partir de 2000.");
+		}
+	}
+
+}
